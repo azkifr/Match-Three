13d4e977835a19e8bf3760e6a1940ed60d4db424
azkifr <azkifr@gmail.com>
2021-09-25 19:15:15 +0700

    Match Three

---
+++ Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour
+{
+    
+    #region Singleton
+    private static GameFlowManager _instance = null;
+    public static GameFlowManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameFlowManager>();
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: GameFLowManager not found");
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public bool IsGameOver { get { return isGameOver; } }
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+    private bool isGameOver = false;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        isGameOver = false;
+    }
+    public void GameOver()
+    {
+        isGameOver =true;
+        ScoreManager.Instance.setHighScore();
+        GameOverUI.Show();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{ 
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
